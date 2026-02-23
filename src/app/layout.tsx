558c783a1b30d1fad64d1eaf864d0185ffbcea78
andrey.zhurba <andrey.zhurba@listat.com.ua>
2026-02-23 15:37:36 +0200

    Complete file recreate

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,34 +1,22 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Inter } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-    title: "Crypto Pulse 2026 | Главное Web3 событие года в Дубае",
-    description: "Присоединяйтесь к крупнейшей конференции по блокчейну и ИИ. Выступления Виталика Бутерина, нетворкинг с китами индустрии и взгляд в будущее технологий.",
+    title: "Crypto Pulse 2026",
+    description: "Web3 Conference in Dubai",
 };
 
 export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
-  return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
-    </html>
-  );
+    children,
+}: {
+    children: React.ReactNode;
+}) {
+    return (
+        <html lang="en">
+            <body className={inter.className}>{children}</body>
+        </html>
+    );
 }
